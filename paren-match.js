4221dc63e9fc90fe02e4de245e303255d639ece1
vashirr <130561897+Vashirr01@users.noreply.github.com>
2024-12-12 16:18:13 +0530

    implemented an isComplete function to check whether the parenthesis match a simple stack(which is just an array) using push and pop methods

--- paren-match.js
+++ paren-match.js
@@ -1,5 +1,39 @@
 function isComplete(string) {
 
+	const stack = []
+
+	for(let i =0; i<string.length; i++){
+		if(string[i] === "{" || string[i] === "[" || string[i] === "("){
+			stack.push(string[i])
+			continue;
+		}
+
+		if(stack.length === 0) {
+		if(string[i] === "}" || string[i] === "]" || string[i] === ")"){
+				return false;
+			}
+		}
+
+		if(stack.length !== 0){
+		if(string[i] === "}") {
+			if(stack.pop() !== "{"){
+			return false;
+			}
+		}
+		if(string[i] === ")") {
+			if(stack.pop() !== "("){
+			return false;
+			}
+		}
+		if(string[i] === "]") {
+			if(stack.pop() !== "["){
+			return false;
+			}
+		}
+		}
+	}
+
+	return true
 }
 
 console.log(isComplete("}"))
