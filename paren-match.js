35f305f85a388670737e879480641f0b215f1615
vashirr <130561897+Vashirr01@users.noreply.github.com>
2025-01-21 21:42:16 +0530

    heavy refactor with added position stack which manages line and column numbers and a match function which makes things simpler than comparing multiple times in the isComplete fn

--- paren-match.js
+++ paren-match.js
@@ -1,37 +1,14 @@
 const fs = require('node:fs');
-const isComplete = require('./paren-match.js')
 const readline = require('node:readline');
-
-const fileStream = fs.createReadStream('./unmatching.js', 'utf8');
-
-const rl = readline.createInterface({
-	input: fileStream,
-	crlfDelay: Infinity,
-
-});
-
-const stack = []
-let lineNo = 0
-
-//fs.readFile('./matching.js', 'utf8', (err, data) =>{
-//	if(err) {
-//		console.error(err)
-//		return;
-//	}
-//	else{
-//		console.log(isComplete(data));
-//	}
-//});
 //
-//fs.readFile('./unmatching.js', 'utf8', (err, data) =>{
-//	if(err) {
-//		console.error(err)
-//		return;
-//	}
-//	else{
-//		console.log(isComplete(data));
-//	}
+//const fileStream = fs.createReadStream('./unmatching.js', 'utf8');
+//
+//const rl = readline.createInterface({
+//	input: fileStream,
+//	crlfDelay: Infinity,
+//
 //});
+
 
 //const content = '\n\n\nwhatsup';
 //
@@ -45,54 +22,94 @@ let lineNo = 0
 //	}
 //})
 //
-//TODO check if it's a non bracket
+
+function matches(open, close){
+	return (
+	(open === "{" && close === "}") ||
+	(open === "[" && close === "]") ||
+	(open === "(" && close === ")")
+	)
+}
+
 function isComplete(string) {
+
+	let stack = []
+	let positionStack = []
+	let lineNo = 1
+	let column = 0
 
 	for(let i =0; i<string.length; i++){
 
+		column++
+		if(string[i] === "\n"){
+			lineNo++
+			column = 0
+			continue
+		}
+
 		if(string[i] === "{" || string[i] === "[" || string[i] === "("){
 			stack.push(string[i])
+			positionStack.push({line: lineNo, column: column})
 			continue;
 		}
 
 		if(stack.length === 0) {
 		if(string[i] === "}" || string[i] === "]" || string[i] === ")"){
-				return false;
+				 console.error(`unmatched parenthesis '${string[i]}' at line ${lineNo} column ${column}`)
 			}
 		}
 
 		if(stack.length !== 0){
-		if(string[i] === "}") {
-			if(stack.pop() !== "{"){
-			return false;
-			}
-		}
-		if(string[i] === ")") {
-			if(stack.pop() !== "("){
-			return false;
-			}
-		}
-		if(string[i] === "]") {
-			if(stack.pop() !== "["){
-			return false;
-			}
+		const lastOpen = stack.pop()
+		const lastPosition = positionStack.pop()
+		if(!matches(string[i], lastOpen)) {
+			console.error(`unmatched parenthesis '${string[i]}' and '${lastOpen}' at line ${lastPosition.line} column ${lastPosition.column}`)
 		}
 		}
 	}
 
-	return stack.length === 0
+	while(stack.length > 0){
+		const lastOpen = stack.pop()
+		const lastPosition = positionStack.pop()
+		console.error(`missing parenthesis for '${lastOpen}' at line '${lastPosition.line}' column '${lastPosition.column}'`)
+	}
 }
 
-rl.on('line', (line) => {
-	console.log(isComplete(line))
-	if(isComplete(line) === false) {
-		console.error(`error at line ${lineNo}`)
-		lineNo++
+fs.readFile('./matching.js', 'utf8', (err, data) =>{
+	if(err) {
+		console.error(err)
+		return;
 	}
-})
+	else{
+		console.log("starting matching.js")
+		isComplete(data);
+		console.log("finished matching.js")
+	}
+});
 
-rl.on('close', () => {
-	console.log("finished");
-})
+fs.readFile('./unmatching.js', 'utf8', (err, data) =>{
+	if(err) {
+		console.error(err)
+		return;
+	}
+	else{
+		console.log("starting unmatching.js")
+		isComplete(data);
+		console.log("finished unmatching.js")
+	}
+});
+
+
+//rl.on('line', (line) => {
+//	console.log(isComplete(line))
+//	if(isComplete(line) === false) {
+//		console.error(`error at line ${lineNo}`)
+//		lineNo++
+//	}
+//})
+//
+//rl.on('close', () => {
+//	console.log("finished");
+//})
 
 
